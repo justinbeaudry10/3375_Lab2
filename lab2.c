2d71616345a5494f4d0516a3f2821ba8f49270e1
justinbeaudry10 <jbeaudr5@uwo.ca>
2022-03-15 14:25:51 -0400

    Completed

--- lab2.c
+++ lab2.c
@@ -19,67 +19,6 @@ typedef struct _a9_timer
     int status;
 } a9_timer;
 
-int main(void)
-{
-    // Pointer to timer
-    volatile a9_timer *const timer = (a9_timer *)MPCORE_PRIV_TIMER;
-    int interval = 2000000; // Interval for counting 1/100th of a sec for 200MHz clock with no prescaler
-
-    int curCount = 0;
-    int lastCount = interval;
-
-    // Initialize timer for 1ms interval
-    timer->load = interval;
-    // Clear any prev timeout flag
-    timer->status = 1;
-    // Start timer for continuous counting
-    timer->control = 3;
-
-    // Initial timer values
-    int ms = 0;
-    int s = 0;
-    int min = 0;
-
-    // Initial lap values
-    int lap_ms = 0;
-    int lap_s = 0;
-    int lap_min = 0;
-
-    // Sets initial display to 00:00:00
-    DisplayHex(ms, s, min);
-
-    // Main loop
-    while (1)
-    {
-        // Wait until timer counts down
-        while (timer->status == 0)
-            ;
-        // Clear timeout flag
-        timer->status = 1;
-
-        if (ms == 99)
-        {
-            ms = 0;
-            if (s == 59)
-            {
-                s = 0;
-                if (min == 59)
-                {
-                    min = 0;
-                }
-                else
-                    min++;
-            }
-            else
-                s++;
-        }
-        else
-            ms++;
-
-        DisplayHex(ms, s, min);
-    }
-}
-
 // Displays number on 7 seg display
 void DisplayHex(int ms, int s, int min)
 {
@@ -113,5 +52,102 @@ int ReadSwitches(void)
 int ReadButton(int btn)
 {
     volatile int *BTN_ptr = (int *)KEY_BASE;
+    // Returns 1 if the given button is pressed, 0 otherwise due to & operator
     return ((*BTN_ptr >> (btn)) & 1);
+}
+
+int main(void)
+{
+    // Pointer to timer
+    volatile a9_timer *const timer = (a9_timer *)MPCORE_PRIV_TIMER;
+    int interval = 2000000; // Interval for counting 1/100th of a sec for 200MHz clock with no prescaler
+
+    // Initialize timer for 1ms interval
+    timer->load = interval;
+
+    int running = 0;
+
+    // Initial timer values
+    int ms = 0;
+    int s = 0;
+    int min = 0;
+
+    // Initial lap values
+    int lap_ms = 0;
+    int lap_s = 0;
+    int lap_min = 0;
+
+    // Sets initial display to 00:00:00
+    DisplayHex(ms, s, min);
+
+    // Main loop
+    while (1)
+    {
+        if (ReadButton(0))
+        {
+            // Clear any prev timeout flag
+            timer->status = 1;
+            // Start timer for continuous counting
+            timer->control = 3;
+            running = 1;
+        }
+        if (ReadButton(1))
+        {
+            // Stop timer
+            timer->control = 0;
+            running = 0;
+        }
+        if (ReadButton(2))
+        {
+            // Set lap time
+            lap_ms = ms;
+            lap_s = s;
+            lap_min = min;
+        }
+        if (ReadButton(3))
+        {
+            // Clear timer
+            ms = 0;
+            s = 0;
+            min = 0;
+        }
+
+        if (running)
+        {
+            // Wait until timer counts down
+            while (timer->status == 0)
+                ;
+            // Clear timeout flag
+            timer->status = 1;
+
+            if (ms == 99)
+            {
+                ms = 0;
+                if (s == 59)
+                {
+                    s = 0;
+                    if (min == 59)
+                    {
+                        min = 0;
+                    }
+                    else
+                        min++;
+                }
+                else
+                    s++;
+            }
+            else
+                ms++;
+        }
+
+        // If first switch is on, display lap time
+        if (ReadSwitches() == 1)
+        {
+            DisplayHex(lap_ms, lap_s, lap_min);
+        }
+        else // Otherwise, display current stopwatch time
+        {
+            DisplayHex(ms, s, min);
+        }
+    }
 }
